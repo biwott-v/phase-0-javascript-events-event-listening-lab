86702c1a76ad9f25e20b3c77434e5a2b8d6c0c82
biwott-v <victor.biwott@student.moringaschool.com>
2025-03-18 23:15:17 +0300

    completes assignment

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-function addingEventListener() {
+let input = document.querySelector("#button");
+function addingEventListener () {
+  input.addEventListener("click",() => alert("I was clicked"))
 }
+
